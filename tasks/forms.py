506b837d819a7a271587f564339caf4f317ce6ec
Apel Mahmud <44221242+apl-mhd@users.noreply.github.com>
2022-01-14 22:12:22 +0600

    frontend design update

--- tasks/forms.py
+++ tasks/forms.py
@@ -6,7 +6,8 @@ from django.forms import ModelForm
 from . models import Task
 
 class TaskForm(forms.ModelForm):
-    
+    title= forms.CharField(widget= forms.TextInput(attrs={'placeholder':'Add new task...'}))
+
     class Meta:
         model = Task
         fields = '__all__'
